c1f6a1b6fec892c23e4843c73a39055dda8f925b
Steve <44811980+SteveRutledge@users.noreply.github.com>
2020-05-29 16:29:28 -0400

    successful retrieval of random summary

--- src/get_wp/console.py
+++ src/get_wp/console.py
@@ -1,11 +1,24 @@
 # src/get_wp/console.py
+
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """The get_wp project"""
-    click.echo("Hulo wurld!")
+    """The get_wp project."""
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
 
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
