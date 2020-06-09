1886bd85a344bf8f7d07ff9ce2d6cc72b84becdf
Steve <44811980+SteveRutledge@users.noreply.github.com>
2020-06-09 15:16:52 -0400

    add language support

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,15 +6,6 @@ import requests
 from get_wp import console
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
-
-@pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
---
+++ tests/test_wikipedia.py
@@ -0,0 +1,9 @@
+# tests/test_wikipedia.py
+
+from get_wp import wikipedia
+
+def test_random_page_uses_given_language(mock_requests_get):
+    wikipedia.random_page(language="de")
+    args, _ = mock_requests_get.call_args
+    assert "de.wikipedia.org" in args[0]
+
