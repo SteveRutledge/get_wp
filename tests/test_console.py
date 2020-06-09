6702160204fceeabb978f97c0033913fb8edf9a7
Steve <44811980+SteveRutledge@users.noreply.github.com>
2020-06-09 14:25:35 -0400

    added pytest, mock and nox along with some unit tests

---
+++ tests/test_console.py
@@ -0,0 +1,41 @@
+# tests/test_console.py
+import click.testing
+import pytest
+
+from get_wp import console
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+def test_main_succeeds(runner, mock_requests_get):
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+def test_main_users_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
