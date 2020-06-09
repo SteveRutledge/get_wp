981f6a0bae0ad7132fbf3cdcc52533efc12a1180
Steve <44811980+SteveRutledge@users.noreply.github.com>
2020-06-09 14:43:52 -0400

    refactored into wikipedia.py

--- src/get_wp/console.py
+++ src/get_wp/console.py
@@ -1,11 +1,9 @@
 # src/get_wp/console.py
 
 import textwrap
-
 import click
 import requests
-
-from . import __version__
+from . import __version__, wikipedia
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
@@ -13,10 +11,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The get_wp project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-
+    data = wikipedia.random_page()
     title = data["title"]
     extract = data["extract"]
 
